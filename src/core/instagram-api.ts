3f92fa51d8c216b4b188ab6919e832a7cc6dabc0
David Adams <david@industrycoder.com>
2023-06-03 21:16:02 +0100

    Update to div

--- src/core/instagram-api.ts
+++ src/core/instagram-api.ts
@@ -29,7 +29,7 @@ const pageContent = new Map<symbol, string>([
   [Unfollow, "//button[text()='Unfollow']"],
   [IsLoggedIn, '//*[@aria-label="Home"]'],
   [AcceptCookies, '//button[contains(text(), "Allow all cookies")]'],
-  [LogIn, "//button[.//text() = 'Log In']"],
+  [LogIn, "//div[.//text() = 'Log In']"],
   [LogInMobile, '//button[contains(text(), "Log In")]'],
   [SaveLoginInfo, '//button[contains(text(), "Save Info")]'],
   [NotificationDialogNotNow, '//button[contains(text(), "Not Now")]'],
